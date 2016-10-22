f32d1894c6a44badff61d7ffe4c6b4d4178d9705
Ivan Lukyanov <greencis@users.noreply.github.com>
2016-10-22 20:56:14 +0300

    Create Properties/AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Wave'n'Click Server")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("greencis")]
+[assembly: AssemblyProduct("Wave_n_Click_Server")]
+[assembly: AssemblyCopyright("Copyright © Ivan Lukyanov 2013-2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00ae4538-4e37-4956-a6a8-34557ad4d96f")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
